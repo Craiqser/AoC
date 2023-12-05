024b34f77c40e65b2b5257cd36f3ea4de88aefd7
Sobolev Sergey <craiqser@gmail.com>
2023-12-05 22:17:13 +0300

    chore: day5, part1

--- 2023/day5/part1.js
+++ 2023/day5/part1.js
@@ -1,8 +1,8 @@
 export const task = async () => {
-	const lines = (await Bun.file('./2023/day5/assets/test1.txt').text()).split('\n');
+	const lines = (await Bun.file('./2023/day5/assets/input.txt').text()).split('\n');
 	const seeds = lines[0].split(':')[1].trim().split(' ').map(Number);
-	const converters = new Map();
-	let groupNum = -1;
+	const converters = new Map(), results = [];
+	let groupNum = 0;
 
 	for (let i = 2; i < lines.length; i++) {
 		const line = lines[i];
@@ -17,7 +17,7 @@ export const task = async () => {
 	}
 	for (const seed of seeds) {
 		let res = seed;
-		for (let i = 0; i < groupNum; i++) {
+		for (let i = 1; i <= groupNum; i++) {
 			const arr = converters.get(i);
 			for (let j = 0; j < arr.length; j++) {
 				if (res >= arr[j][1] && res < arr[j][1] + arr[j][2]) {
@@ -25,15 +25,8 @@ export const task = async () => {
 					break;
 				}
 			}
-			console.log(res);
 		}
-		break;
+		results.push(res);
 	}
-	// console.log(convertations);
+	console.log(Math.min(...results));
 };
-/*
-Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
-Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
-Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
-Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
-*/
