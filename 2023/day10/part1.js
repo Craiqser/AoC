fb1620118e4657f1877f09a3f8dea95f760ee10d
Sobolev Sergey <craiqser@gmail.com>
2023-12-23 18:10:33 +0300

    refactor: folder names

--- 2023/day10/part1.js
+++ 2023/day10/part1.js
@@ -1,5 +1,7 @@
 export const task = async () => {
 	const lines = (await Bun.file('./2023/day10/assets/test1.txt').text()).split('\n');
+	lines.map((line, i) => lines[i] = line.split(''));
+	const p = [0, 0];
 
 	console.dir(lines);
 };
