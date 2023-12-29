6165b06d4f4f284c57deea415b058dcc985472ca
Sobolev Sergey <craiqser@gmail.com>
2023-12-29 23:00:14 +0300

    feat: day16, part2

---
+++ 2023/day16/part2.js
@@ -0,0 +1,68 @@
+export const task = async () => {
+	const map = (await Bun.file('./2023/day16/assets/input.txt').text()).split('\n').map((row) => row.split(''));
+	const points = [];
+	let maxRes = 0;
+	for (let i = 0; i < map.length; i++) {
+		points.push([i, 0, 0, 1]);
+		points.push([i, map[0].length - 1, 0, -1]);
+	}
+	for (let i = 0; i < map[0].length; i++) {
+		points.push([0, i, 1, 0]);
+		points.push([map.length - 1, i, -1, 0]);
+	}
+
+	while (points.length) {
+		const queue = [points.pop()];
+		const passed = new Set();
+		const res = new Set();
+
+		while (queue.length) {
+			const [row, col, drow, dcol] = queue.pop();
+			const key = `${row},${col},${drow},${dcol}`;
+			if (passed.has(key)) continue;
+
+			passed.add(key);
+			res.add(`${row},${col}`);
+			const next = [];
+
+			switch(map[row][col]) {
+				case '/':
+					next.push([-dcol, -drow]);
+					break;
+				case '\\':
+					next.push([dcol, drow]);
+					break;
+				case '|':
+					if (dcol) {
+						next.push([-1, 0]);
+						next.push([1, 0]);
+					} else {
+						next.push([drow, dcol]);
+					}
+					break;
+				case '-':
+					if (drow) {
+						next.push([0, 1]);
+						next.push([0, -1]);
+					} else {
+						next.push([drow, dcol]);
+					}
+					break;
+				default:
+					next.push([drow, dcol]);
+					break;
+			}
+
+			next.forEach((v) => {
+				const nrow = row + v[0], ncol = col + v[1];
+				if (nrow > -1 && nrow < map.length && ncol > -1 && ncol < map[0].length) {
+					queue.push([nrow, ncol, v[0], v[1]]);
+				}
+			});
+		}
+		if (res.size > maxRes) {
+			maxRes = res.size;
+		}
+	}
+	console.log(maxRes)
+};
