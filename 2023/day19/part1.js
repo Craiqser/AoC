ce41d558c79cdffa3038368313773cbee79189b5
Sobolev Sergey <craiqser@gmail.com>
2024-01-14 11:46:32 +0300

    feat: day19, part1

--- 2023/day19/part1.js
+++ 2023/day19/part1.js
@@ -1,14 +1,36 @@
 export const task = async () => {
-	const lines = (await Bun.file('./2023/day19/assets/test1.txt').text()).split('\n');
-	console.log(lines);
-}
-/*
-All parts begin in the workflow named `in`.
-{x=787,m=2655,a=1222,s=2876}: in -> qqz -> qs -> lnx -> A
-{x=1679,m=44,a=2067,s=496}: in -> px -> rfg -> gd -> R
-{x=2036,m=264,a=79,s=2244}: in -> qqz -> hdj -> pv -> A
-{x=2461,m=1339,a=466,s=291}: in -> px -> qkq -> crn -> R
-{x=2127,m=1623,a=2188,s=1013}: in -> px -> rfg -> A
+	const data = (await Bun.file('./2023/day19/assets/input.txt').text()).split('\n\n');
+	const wf = data[0].split('\n');
+	const workflows = {};
+	wf.map((workflow) => {
+		const idx = workflow.indexOf('{');
+		const steps = workflow.slice(idx + 1, -1).split(',').map((step) => {
+			if (step.includes(':')) {
+				const act = step.split(':');
+				const func = act[0].includes('<')
+					? (part) => part[act[0].split('<')[0]] < act[0].split('<')[1] ? act[1] : ''
+					: (part) => part[act[0].split('>')[0]] > act[0].split('>')[1] ? act[1] : '';
+				return func;
+			} else return (_) => step;
+		});
+		workflows[workflow.slice(0, idx)] = steps;
+	});
 
-Ultimately, three parts are accepted. Adding up the x, m, a, and s rating for each of the accepted parts gives 7540 for the part with x=787, 4623 for the part with x=2036, and 6951 for the part with x=2127. Adding all of the ratings for all of the accepted parts gives the sum total of 19114.
-*/
+	const parts = data[1].split('\n').map((part) =>
+		part.slice(1, -1).split(',').reduce((acc, category) => {
+			const val = category.split('=');
+			acc[val[0]] = parseInt(val[1]);
+			return acc;
+		}, {}));
+
+	const process = (part, step) => {
+		if (step === 'R') return 0;
+		if (step === 'A') return Object.values(part).reduce((acc, val) => acc += val, 0);
+		for (let i = 0; i < workflows[step].length; i++) {
+			if (workflows[step][i](part).length) {
+				return process(part, workflows[step][i](part));
+			}
+		}
+	};
+	console.log(parts.reduce((acc, part) => acc += process(part, 'in'), 0));
+}
