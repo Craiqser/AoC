5b652652e816f2ced2ae7eeb2e8963ad4290ab71
Sobolev Sergey <craiqser@gmail.com>
2024-01-23 17:17:25 +0300

    chore: day23, part2 slow, part2 fast init

--- 2023/day23/part2.js
+++ 2023/day23/part2.js
@@ -1,29 +1,7 @@
 export const task = async () => {
-	const lines = (await Bun.file('./2023/day23/assets/test1.txt').text()).split('\n');
+	const lines = (await Bun.file('./2023/day23/assets/input.txt').text()).split('\n');
 	let res = 0;
 	const queue = [[0, 1, 0, ['0-1']]];
-	const next = (row, col, step, path) => {
-		const key = `${row}-${col}`;
-		if (path.includes(key)) return;
-		queue.push([row, col, ++step, [...path, key]]);
-	};
-	while (queue.length) {
-		const [row, col, step, path] = queue.pop();
-		if ((row > 0) && ('.^'.includes(lines[row - 1][col])))
-			next(row - 1, col, step, path);
 
-		if ((row < lines.length - 1) && ('.v'.includes(lines[row + 1][col])))
-			next(row + 1, col, step,  path);
-
-		if ((col > 0) && ('.<'.includes(lines[row][col - 1])))
-			next(row, col - 1, step, path);
-
-		if ((col < lines[0].length - 1) && ('.>'.includes(lines[row][col + 1])))
-			next(row, col + 1, step, path);
-
-		if ((row === lines.length - 1) && (col === lines[0].length - 2))
-			res = Math.max(res, step);
-	}
 	console.log(res);
 }
-// 154
